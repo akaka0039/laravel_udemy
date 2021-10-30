d1ea555bb2d16859bde9d4c660d742fdbb413123
aka0039 <yumeno.13579@gmail.com>
2021-10-30 14:55:53 +0900

    slot with name

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,8 @@
 
 <!-- 20211024_how to use Component -->
+
 <!-- x-folder's name _ file's name -->
 <x-tests.app>
+    <x-slot name="header">ヘッダーです</x-slot>
 component-test1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,5 @@
 <x-tests.app>
+    <!-- 20211030_名前付きスロットの挿入 -->
+<x-slot name="header">ヘッダーです</x-slot>
 component-test2
 </x-tests.app>

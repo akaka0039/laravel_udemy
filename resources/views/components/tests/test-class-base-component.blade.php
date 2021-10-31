1f7bb7c6b68054c7386238c409d4133895a7e746
aka0039 <yumeno.13579@gmail.com>
2021-10-31 13:03:58 +0900

    classBase of component

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,8 @@
+<div>
+    クラスベースのコンポーネントです。
+
+{{-- クラスベースの初期化 --}}
+<div>{{ $classBaseMessage }}</div>
+<div>{{ $defaultMessage }}</div>
+    <!-- When there is no desire, all things are at peace. - Laozi -->
+</div>

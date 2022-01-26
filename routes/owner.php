fda39d84a7425aa71692db5f31c5c0b00d429aab
aka0039 <yumeno.13579@gmail.com>
2022-01-26 11:30:22 +0900

    add migrate image

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,6 +47,10 @@ Route::prefix('shops')->middleware(['auth:owners'])->group(function () {
         'update'
     ])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+    ->middleware('auth:owners')->except(['show']);
+
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');

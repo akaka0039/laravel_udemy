1a6d677e881392e02df8909780e581e3a29a7104
aka0039 <yumeno.13579@gmail.com>
2022-01-26 10:58:40 +0900

    edit

---
+++ resources/views/components/shop-thumbnail.blade.php
@@ -0,0 +1,8 @@
+{{-- サムネイルの表示（分岐） --}}
+<div>
+    @if(empty($shop->filename))
+        <img src="{{ asset('images/no_image.jpg')}}">
+    @else
+        <img src="{{ asset('storage/shops/'. $shop->filename )}}">
+    @endif
+</div>
--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -10,20 +10,58 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                    <form method="post" action="{{ route('owner.shops.update', ['shop' => $shop->id ])}}"  enctype="multipart/form-data">
+                    <form method="post" action="{{ route('owner.shops.update', ['shop' => $shop->id]) }}"
+                        enctype="multipart/form-data">
                         @csrf
                         <div class="-m-2">
-                          <div class="p-2 w-1/2 mx-auto">
-                            <div class="relative">
-                              <label for="image" class="leading-7 text-sm text-gray-600">images</label>
-                              <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg"  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            {{-- 店舗名 --}}
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="name" class="leading-7 text-sm text-gray-600">Shop Name</label>
+                                    <input type="text" id="name" name="name" value="{{ $shop->name }}" required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
                             </div>
-                          </div>
-                        </div>
-                    
-                        <div class="p-2 w-full flex justify-around mt-4">
-                            <button type="button" onclick="location.href='{{route('owner.shops.index')}}'"  class=" bg-gray-300 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">back</button>
-                            <button type="submit" class=" text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">更新する</button>
+                            {{-- 店舗情報 --}}
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="information" class="leading-7 text-sm text-gray-600">Shop
+                                        information</label>
+                                    <textarea id="information" name="information" rows="10" required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $shop->information }}</textarea>
+                                </div>
+                            </div>
+                            {{-- サムネイル --}}
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <div class="w-32">
+                                        <x-shop-thumbnail />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="image" class="leading-7 text-sm text-gray-600">images</label>
+                                    <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            {{-- ラジオボタン_now on sell or not --}}
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+                                    <div><input type="radio" name="is_selling" value="1" class="mr-2"
+                                            @if ($shop->is_selling === 1){checked} @endif>販売中</div>
+                                    <div><input type="radio" name="is_selling" value="0" class="mr-2"
+                                            @if ($shop->is_selling === 0){checked} @endif>停止中</div>
+                                </div>
+                            </div>
+                            {{-- ボタン　update or back --}}
+                            <div class="p-2 w-full flex justify-around mt-4">
+                                <button type="button" onclick="location.href='{{ route('owner.shops.index') }}'"
+                                    class=" bg-gray-300 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">back</button>
+                                <button type="submit"
+                                    class=" text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">update</button>
+                            </div>
                         </div>
                     </form>
                 </div>

d2c6c509e757e943219b818096a7eadce19da12d
aka0039 <yumeno.13579@gmail.com>
2022-01-31 09:52:33 +0900

    product index, eager loading

--- app/Http/Controllers/Admin/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Admin/Auth/RegisteredUserController.php
@@ -33,9 +33,10 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:admin'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:admins'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 

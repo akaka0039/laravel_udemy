613dce1bc5e3af08f547e1aa336c229497f76010
aka0039 <yumeno.13579@gmail.com>
2022-02-07 11:43:56 +0900

    在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Stock;
 
 use Illuminate\Support\Facades\DB;
+
+
 
 
 class ItemController extends Controller
@@ -77,7 +80,13 @@ class ItemController extends Controller
     {
 
         $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+            ->sum('quantity');
 
-        return view('user.show', compact('product'));
+        if ($quantity > 9) {
+            $quantity = 9;
+        }
+
+        return view('user.show', compact('product', 'quantity'));
     }
 }
